58d41ee766143ca018824c771bd0ffa429ef60c8
airuma <airuma2048@gmail.com>
2026-06-27 01:37:50 +0800

    为增加其他功能模块做准备

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# thought-page
+# my-dashboard
+
+一个基于 GitHub Pages 的个人仪表盘框架。
+
+## 模块
+
+- `modules/thoughts/`: 碎片想法记录功能，数据同步到私有数据仓库。
---
+++ modules/thoughts/manifest.js
@@ -0,0 +1,9 @@
+(function registerThoughtsModule() {
+  window.dashboardModules = window.dashboardModules || [];
+  window.dashboardModules.push({
+    id: "thoughts",
+    title: "碎片想法",
+    description: "快速记录，随时搜索，数据同步到你的 GitHub 私有仓库。",
+    href: "modules/thoughts/",
+  });
+}());
